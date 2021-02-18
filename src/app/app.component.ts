8d585b4880e9a022087ce4b6b7b5de6a1a2e060c
joelstevick <joelstevick@outlook.com>
2021-02-18 11:25:34 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,30 @@
-import { Component, VERSION } from "@angular/core";
+import { Component, OnInit, VERSION } from "@angular/core";
+
+const PageSize = 10;
 
 @Component({
   selector: "my-app",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  items: any[] = [
-    {
-      html: "this is a test"
-    }
-  ];
+export class AppComponent implements OnInit {
+  items: any[] = [];
+  count = 0;
 
+  ngOnInit(): void {
+    this.fetchMore();
+  }
+
+  fetchMore() {
+    const newItems: any[] = [];
+    for (let i = 0; i < PageSize; i++) {
+      newItems.push({
+        html: `${-1 * (this.items.length + i)}`
+      });
+    }
+    this.items = [...newItems, ...this.items];
+    this.items.reverse();
+  }
   vsStart(info) {
     console.log(info);
   }

47223f404c2d383f14c9e6a5245b208a0a7f09a3
joelstevick <joelstevick@outlook.com>
2021-02-18 12:02:37 -0500

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,34 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
-import { VirtualScrollerModule } from "ngx-virtual-scroller";
+import {
+  VirtualScrollerDefaultOptions,
+  VirtualScrollerModule
+} from "ngx-virtual-scroller";
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
+
+export function vsDefaultOptionsFactory(): VirtualScrollerDefaultOptions {
+  return {
+    checkResizeInterval: 1000,
+    modifyOverflowStyleOfParentScroll: true,
+    resizeBypassRefreshThreshold: 5,
+    scrollAnimationTime: 750,
+    scrollDebounceTime: 100,
+    scrollThrottlingTime: 0,
+    stripedTable: false
+  };
+}
 
 @NgModule({
   imports: [BrowserModule, FormsModule, VirtualScrollerModule],
   declarations: [AppComponent, HelloComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: "virtual-scroller-default-options",
+      useFactory: vsDefaultOptionsFactory
+    }
+  ]
 })
 export class AppModule {}

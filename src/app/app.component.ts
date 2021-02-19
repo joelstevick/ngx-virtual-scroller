1f1a954cd201f43647f88fd708483726ea6f7e5c
joelstevick <joelstevick@outlook.com>
2021-02-19 16:20:49 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
-import { Component, OnInit, VERSION, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { DomSanitizer } from "@angular/platform-browser";
 import { IPageInfo, VirtualScrollerComponent } from "ngx-virtual-scroller";
 
-const PageSize = 1;
-
-const MaxItems = Infinity;
+const MaxItems = 1000;
 
 @Component({
   selector: "my-app",
@@ -20,61 +18,27 @@ export class AppComponent implements OnInit {
   count = 0;
   timeout: any;
   initialized = false;
-  beaconEl: HTMLElement;
 
   ngOnInit(): void {
-    this.beaconEl = document.querySelector("#beacon") as HTMLElement;
-
-    // listen for scroll to top events
-    const scrollArea = document.querySelector("virtual-scroller");
-
-    const observer = new IntersectionObserver(
-      (event: IntersectionObserverEntry[]) => {
-        if (
-          (!this.initialized || this.isBeaconDisplayed()) &&
-          event[0].isIntersecting
-        ) {
-          console.log("top", this.items.length);
-          this.displayBeacon(false);
-          this.fetchMore();
-        }
-      },
-      {
-        root: scrollArea
-      }
-    );
-
-    observer.observe(this.beaconEl);
-  }
-
-  isBeaconDisplayed() {
-    return this.beaconEl.style.visibility === "visible";
-  }
-  displayBeacon(show: boolean) {
-    this.beaconEl.style.visibility = show ? "visible" : "hidden";
-  }
-  fetchMore() {
-    const pageSize = this.items.length > 0 ? PageSize : 10;
-
-    const base = this.items.length;
-
-    for (let i = 0; i < pageSize; i++) {
-      this.items.unshift({
+    for (let i = 0; i < MaxItems; i++) {
+      this.items.push({
         html: this.sanitizer.bypassSecurityTrustHtml(
-          `<div style="height: ${this.getHeight()}px">${base + i}</div>`
+          `<div style="height: ${this.getHeight()}px">${MaxItems - i}</div>`
         )
       });
     }
   }
+
   vsStart(pageInfo: IPageInfo) {
-    console.log("vsStart", pageInfo);
-    if (pageInfo.startIndex >= 0) {
-      this.displayBeacon(true);
-    }
-    if (!this.initialized && this.items.length > 0) {
+    return;
+    if (!this.initialized) {
       this.initialized = true;
-      this.virtualScroller.scrollToIndex(this.items.length - 1);
+      this.virtualScroller.scrollToIndex(MaxItems - 1);
     }
+    if (pageInfo.startIndex < 900) {
+      this.items = this.items.slice(900);
+    }
+    console.log("vsStart", pageInfo);
   }
 
   getHeight() {
